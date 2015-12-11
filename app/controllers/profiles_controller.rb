89e6c2e6eed39194a6b7d3ae4bbe24c2b2dbf576
Caleb White <calebjwhite@gmail.com>
2015-12-11 17:55:54 +0000

    changed Profile.new in profilescontroller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,23 @@ class ProfilesController < ApplicationController
     def new
         # forom each user fills out own profile
         @user = User.find( params[:user_id] )
-        @profile = @user.build_profile
-        @variable = params[:hello]
+        @profile = Profile.new
     end
+    
+    def create
+       @user = User.find( params[:user_id] )
+       @profile = @user.build_profile(profile_params)
+        if @profile.save
+            flash[:success] = "Profile Update Complete!"
+            redirect_to user_path(params[:user_id] )
+        else
+            render action: :new
+        end
+    end
+    
+    
+    private
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number ,:contact_email, :description)
+        end
 end
