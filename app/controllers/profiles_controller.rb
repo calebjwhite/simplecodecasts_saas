4d4a9ba49827d9ffdac3abca17ca91635ee90e3c
Caleb White <calebjwhite@gmail.com>
2015-12-11 00:27:53 +0000

    Added Profiles controller and new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        # forom each user fills out own profile
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile
+        @variable = params[:hello]
+    end
 end
